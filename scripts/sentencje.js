a10a658b117504599888dbda35d4f76dd6af1f37
MartynaB13 <83344794+MartynaB13@users.noreply.github.com>
2021-05-26 11:57:17 +0200

    Update sentencje.js

--- scripts/sentencje.js
+++ scripts/sentencje.js
@@ -1,5 +1,5 @@
 function sentencja() {
-    var sent = ['Kogut myslal o niedzieli, a w sobote leb ucieli', 'Kto w poniedzialek przeproznuje, we wtorek nie bedzie miaĹ co jesc', 'Ostatni wtorek wskazuje, jaka pogoda w poscie panuje', 'Wenus jskoko jedyny obraca się w przeciwną stronę, czyli wstecznie do kierunku Słońca. ', 'Nie pij Piotrek, nie pij w czwartek. Picie w czwartek nic nie warte', 'W piatek zly poczatek', 'Przy sobocie po robocie'];
+    var sent = ['Astronauci na Międzynarodowej Stacji Kosmicznej obserwują w ciągu jednej doby 16 wschodów i zachodów Słońca', 'Słońce jest ponad 300 000 razy większe niż ziemia', 'Wenus jest najgorętszą planetą w naszym układzie słonecznym o temperaturze powierzchni ponad 450 stopni celcjusza', 'Wenus jskoko jedyny obraca się w przeciwną stronę, czyli wstecznie do kierunku Słońca. ', 'Zachód słońca na Marsie wydaje się niebieski', 'Cztery największe księżyce Jowisza nazwane są Europa, Ganymede, Callisto i Io', 'Wielu naukowców uważa, że uderzenie asteroidów spowodowało wymarcie dinozaurów około 65 milionów lat temu'];
     var nazwy_dni = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
     var tydz = new Date().getDay();
     document.getElementById("powiedzenia").innerHTML = "Dzisiejsza data: " + nazwy_dni[tydz] + ", a sentencja na dzis, to: " + sent[tydz];
